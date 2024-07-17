b691b5e4dd944633f6d94c7b08b8c190096ece1c
samcovert13@gmail.com <samcovert13@gmail.com>
2024-07-16 20:57:11 -0600

    homepage

--- react-vite/src/components/Homepage/Homepage.jsx
+++ react-vite/src/components/Homepage/Homepage.jsx
@@ -1,14 +1,15 @@
-
+import './Homepage.css'
 
 const Homepage = () => {
     return (
-        <>
-        <h1>In Memory of The Arizona Coyotes</h1>
-        <h4>Gone but never forgotten</h4>
-        <p>Yotes 4 Ever is dedicated to keeping the memory of the
-            Phoenix/Arizona Coyotes.
+        <div className="homepage-container">
+        <h1 className="home-title">Gone But Never Forgotten</h1>
+        <h4 className="home-sub">In memory of the Phoenix/Arizona Coyotes</h4>
+        <p className="home-description">Yotes 4 Ever is dedicated to keeping the memory of the
+            Phoenix/Arizona Coyotes alive. Click the profile button to create a profile so you can join
+             the Coyotes Community in buying/selling items, sharing news, and memories of our beloved Desert Dawgs.
         </p>
-        </>
+        </div>
     )
 }
 
